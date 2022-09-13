2e61f01e1f33db6e0e13e44e912f3ea92a9e3a60
Dylan(Phạm Thành Công) <cong.pham@lginnotek.com>
2022-09-13 18:00:31 +0700

    import template

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monitormachines.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MonitorLots.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
